cad3335d32d4e1941204c13ef50d2f0b3ce60f70
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-25 02:46:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,30 +5,30 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/' do 
+    redirect to "/recipes"
+  end 
+  
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
 
-  # Form to create a new recipe
   get '/recipes/new' do
     erb :new
   end
 
-  # Post the new recipe
   post '/recipes' do
     @recipe = Recipe.create(params)
     puts params
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  # Show new recipe
   get '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
     erb :show
   end
 
-  # Edit the recipe
   get '/recipes/:id/edit' do
     @recipe = Recipe.find(params[:id])
     erb :edit
