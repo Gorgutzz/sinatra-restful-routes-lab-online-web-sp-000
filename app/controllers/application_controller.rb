4b838de484bd02bac62394189250bc053e2bfd1b
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-25 00:29:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,41 +5,41 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-  get '/recipes' do 
+  get '/recipes' do
       #gonna need to access all the recipes here!
-      @recipes = Recipe.all 
+      @recipes = Recipe.all
       erb :index
-    end 
-  
-    get '/recipes/new' do 
-      erb :new 
-    end 
-  
-    post '/recipes' do 
+    end
+
+    get '/recipes/new' do
+      erb :new
+    end
+
+    post '/recipes' do
       recipe = Recipe.create(params)
       redirect to "/recipes/#{recipe.id}"
-    end 
-  
-    get '/recipes/:id' do 
+    end
+
+    get '/recipes/:id' do
       @recipe = Recipe.find(params[:id])
       erb :show
-    end 
-  
-    get '/recipes/:id/edit' do 
+    end
+
+    get '/recipes/:id/edit' do
       @recipe = Recipe.find(params[:id])
-      erb :edit 
-    end 
-  
-    patch '/recipes/:id' do 
+      erb :edit
+    end
+
+    patch '/recipes/:id' do
       recipe = Recipe.find(params[:id])
       recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
       redirect to "/recipes/#{recipe.id}"
-    end 
-  
-    delete '/recipes/:id' do 
+    end
+
+    delete '/recipes/:id' do
       recipe = Recipe.find(params[:id])
-      recipe.destroy 
+      recipe.destroy
       redirect to "/recipes"
-    end 
+    end
 
 end
