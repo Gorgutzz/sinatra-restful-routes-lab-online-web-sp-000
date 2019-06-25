94c7a2d2b14e9e8872a8b9a58a1281609d105df3
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-25 02:58:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    @recipe = Recipe.create(params)
-    puts params
-    redirect to "/recipes/#{@recipe.id}"
+    recipe = Recipe.create(params)
+    redirect to "/recipes/#{recipe.id}"
   end
 
   get '/recipes/:id' do
@@ -31,12 +30,9 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
-    @recipe.name = params[:name]
-    @recipe.ingredients = params[:ingredients]
-    @recipe.cook_time = params[:cook_time]
-    @recipe.save
-    redirect to "/recipes/#{@recipe.id}"
+    recipe = Recipe.find(params[:id])
+    recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect to "/recipes/#{recipe.id}"
   end
 
   delete '/recipes/:id' do
