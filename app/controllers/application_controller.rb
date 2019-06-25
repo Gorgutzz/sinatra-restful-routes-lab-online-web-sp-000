c2991750c0a63bc3ddad5c6fcb55dd7f68cbb477
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-25 00:29:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,39 +5,41 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-  get '/recipes' do
-    @recipes = Recipe.all
-    erb :index
-  end
-
-  get '/recipes/new' do
-    erb :new
-  end
-
-  post '/recipes/new' do
-    recipe = Recipe.create(params)
-    redirect "/recipes/#{recipe.id}"
-  end
-
-  get '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
-    erb :show
-  end
-
-  get '/recipes/:id/edit' do
-    @recipe = Recipe.find(params[:id])
-    erb :edit
-  end
-
-  patch '/recipes/:id/edit' do
-    Recipe.update(params[:id], name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-    redirect "/recipes/#{params[:id]}"
-  end
-
-  delete '/recipes/:id' do
-    Recipe.delete(params[:id])
-    redirect '/recipes'
-  end
-
+  get '/recipes' do 
+      #gonna need to access all the recipes here!
+      @recipes = Recipe.all 
+      erb :index
+    end 
+  
+    get '/recipes/new' do 
+      erb :new 
+    end 
+  
+    post '/recipes' do 
+      recipe = Recipe.create(params)
+      redirect to "/recipes/#{recipe.id}"
+    end 
+  
+    get '/recipes/:id' do 
+      @recipe = Recipe.find(params[:id])
+      erb :show
+    end 
+  
+    get '/recipes/:id/edit' do 
+      @recipe = Recipe.find(params[:id])
+      erb :edit 
+    end 
+  
+    patch '/recipes/:id' do 
+      recipe = Recipe.find(params[:id])
+      recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+      redirect to "/recipes/#{recipe.id}"
+    end 
+  
+    delete '/recipes/:id' do 
+      recipe = Recipe.find(params[:id])
+      recipe.destroy 
+      redirect to "/recipes"
+    end 
 
 end
